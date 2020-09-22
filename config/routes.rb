67f8942c1c65e99877adcc3e540c15ab2055e30d
jiayeehiew <girlhiew@gmail.com>
2020-09-22 17:42:11 +0800

    scaffold crud articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
+
   devise_for :users, :controllers => { registrations: 'registrations' } 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  resources :articles
+    
   root to: 'pages#index'
   get 'pages/contact'
   get 'pages/about'
