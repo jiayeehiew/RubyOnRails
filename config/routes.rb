1f6be7edc2e61e6789877b5a5c72036bd5fdecaf
jiayeehiew <girlhiew@gmail.com>
2020-09-25 11:37:36 +0800

    added controller for comments and view partial

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' } 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+
   resources :contacts
   root to: 'pages#index'
   get 'pages/contact'
